565c963a241487fcc8f31ffcc88cebcb0f600299
jhonchester <jhonchesterguijoba186@gmail.com>
2024-10-22 16:01:14 +0800

    footer updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,38 @@
 <footer class="bg-white text-gray-600 border-t border-gray-100 text-center">
-    <div class="p-3">
-        &copy; 2024 Copyright
-    </div>
+    <style>
+        .team_logo {
+            margin-right: 50px; /* Space between logo and copyright text */
+        }
+        .social-icon {
+            width: 120px; /* Size of social media icons */
+            margin-right: 50px; /* Space between icons */
+        }
+        /* Optional: Remove margin from the last social icon */
+        .social-icon:last-child {
+            margin-right: 0; /* Remove margin for the last icon */
+        }
+    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
+    <div class="container p-3 d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center">
+                <img src="{{ asset('images/logos/log.png') }}" alt="Logo" style="width: 50px;" class="team_logo">
+                <span class="team_text">&copy; 2024 Array Out of Bounds. All Rights Reserved</span>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/chesterguijoba" class="text-gray-600 social-icon" target="_blank">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="https://www.instagram.com/jhonchester__/" class="text-gray-600 social-icon" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://github.com/jhonchester" class="text-gray-600 social-icon" target="_blank">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="https://www.threads.net/@jhonchester__?xmt=AQGzhT-_9UmX-xvctu8LMjfYm1e-V-QmD4R1ebhh10hTDbY" class="text-gray-600 social-icon" target="_blank">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </div>
+        </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,60 @@
 @extends('layouts.front')
 
 @section('meta')
-<meta name="description" content="about the website"
-
-{{--.....--}}
-
+<meta name="description" content="Spartan Commerce is an eCommerce platform offering quality products at affordable prices.">
 @endsection
 
 @section('title')
-<title>Spartan Commerce</title>
 
 @endsection
 
 @section('style')
+<!-- Bootstrap CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 <style>
-    /*styles*/
+    .hero {
+        background-color: #f5f5f5;
+        padding: 50px 0;
+    }
+    .hero-title {
+        font-size: 2.5rem;
+        color: #333;
+    }
+    .text-secondary {
+        font-size: 1rem;
+        color: #777;
+        max-width: 50%;
+    }
+    .button-space {
+        margin-right: 20px; /* Adjust spacing as needed */
+    }
+    
 </style>
-
 @endsection
 
 @section('content')
-<h1>Home Page</h1>
-
+<section class="hero">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="hero-title fw-bold">
+                    Spartan Commerce
+                </div>
+                <p class="mt-3 text-secondary">
+                    Welcome to Spartan Commerce! We bring you the best deals on a wide range of products. Explore our catalog and enjoy an easy and secure shopping experience.
+                </p>
+            </div>
+        </div>
+        <div class="cta mt-5 d-flex">
+            <a href="" class="btn btn-primary border-0 shadow-none button-space">Shop Now</a> <!-- Added custom class here -->
+            <a href="" class="btn btn-light shadow">Contact Us</a>
+        </div>
+    </div>
+</section>
 @endsection
-
-
 
 @section('script')
 <script>
-    /*script*/
+    // Custom JavaScript can be added here
 </script>
-    
-
-
 @endsection
