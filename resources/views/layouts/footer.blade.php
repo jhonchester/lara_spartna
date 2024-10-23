959090b219da382a48490fca57ecf10db60be01d
jhonchester <jhonchesterguijoba186@gmail.com>
2024-10-23 19:57:55 +0800

    updated footer and hero page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,38 +1,102 @@
 <footer class="bg-white text-gray-600 border-t border-gray-100 text-center">
     <style>
-        .team_logo {
-            margin-right: 50px; /* Space between logo and copyright text */
+        @import url("https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
+        /* global styles */
+
+        * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "League Spartan", sans-serif;
         }
-        .social-icon {
-            width: 120px; /* Size of social media icons */
-            margin-right: 50px; /* Space between icons */
+        
+        #footer {
+        padding: 75px 0; /* Remove inline padding */
+        justify-content: space-evenly;
         }
-        /* Optional: Remove margin from the last social icon */
-        .social-icon:last-child {
-            margin-right: 0; /* Remove margin for the last icon */
+
+        .footer-logo {
+        width: 100px; /* Adjusted width to resize logo */
+        height: auto; /* Keeps the aspect ratio */
+        margin: 5px; /* Reduce space between logos */
+        }
+
+        .socials a {
+        font-size: 24px; /* Adjust icon size */
+        }
+
+        .about {
+        text-align: center; /* Center the text */
+        }
+
+        .about-logo {
+        width: 100px; /* Set a uniform width for logos */
+        margin: 15px; /* Reduce space between logos */
+        height: auto; /* Maintain aspect ratio */
+        }
+
+        p {
+        margin: 2px 0; /* Reduce space between paragraphs */
+        font-size: 14px; /* Adjust font size */
+        }
+        
+        .socials a {
+        color: grey;
+        margin: 0 5px; /* Adjust this value to increase/decrease the horizontal space between logos */
+        }
+
+        .socials img {
+        width: 30px; /* Adjust size if needed */
+        height: auto;
         }
     </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    
-    <div class="container p-3 d-flex justify-content-between align-items-center">
-            <div class="d-flex align-items-center">
-                <img src="{{ asset('images/logos/log.png') }}" alt="Logo" style="width: 50px;" class="team_logo">
-                <span class="team_text">&copy; 2024 Array Out of Bounds. All Rights Reserved</span>
-            </div>
-            <div>
-                <a href="https://www.facebook.com/chesterguijoba" class="text-gray-600 social-icon" target="_blank">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="https://www.instagram.com/jhonchester__/" class="text-gray-600 social-icon" target="_blank">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a href="https://github.com/jhonchester" class="text-gray-600 social-icon" target="_blank">
-                    <i class="fab fa-github"></i>
-                </a>
-                <a href="https://www.threads.net/@jhonchester__?xmt=AQGzhT-_9UmX-xvctu8LMjfYm1e-V-QmD4R1ebhh10hTDbY" class="text-gray-600 social-icon" target="_blank">
-                    <i class="fab fa-twitter"></i>
-                </a>
+    <script
+      src="https://kit.fontawesome.com/0e53af926d.js"
+      crossorigin="anonymous"
+    ></script>
+    <div id="footer" class="container-fluid bg-light py-5">
+    <div class="row">
+        <div class="col-md-6 contact text-center text-md-start">
+            <a href="index.html">
+                <img src="assets/logo.png" alt="" class="footer-logo"/>
+            </a>
+            <a href="index.html">
+                <img src="assets/rgo.jpg" alt="" class="footer-logo"/>
+            </a>
+            <h3 class="mt-4">Contact RGO JPLPC Malvar</h3>
+            <address>
+                <p><strong>Address:</strong> Poblacion Malvar Batangas</p>
+                <p><strong>Phone:</strong> (+63) 9107029382</p>
+                <p><strong>Hours:</strong> 8:00am - 5:00pm Monday - Friday</p>
+            </address>
+            <h3 class="soc">Follow Us</h3>
+            <div class="socials mt-3">
+                <a href="#" class="me-2"><i class="fa-brands fa-facebook-square"></i></a>
+                <a href="#" class="me-2"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#" class="me-2"><i class="fa-brands fa-telegram"></i></a>
+                <a href="#" class="me-2"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
             </div>
         </div>
+        <div class="col-md-6 about text-center mt-4 mt-md-0">
+            <h3>In Partnership with</h3>
+            <div class="d-flex justify-content-center flex-wrap">
+                <img src="https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo.png" alt="" class="about-logo me-2"/>
+                <img src="https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo.png" alt="" class="about-logo me-2"/> 
+                <img src="https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo.png" alt="" class="about-logo me-2"/>  
+                <img src="https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo.png" alt="" class="about-logo me-2"/>
+                <img src="https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo.png" alt="" class="about-logo me-2"/>
+            </div>
+            <p>College of Arts and Sciences</p>
+            <p>College of Engineering and Technology</p>
+            <p>College of Accounting, Business, Hospitality, Entrepreneurship, and International Hospitality Management</p>
+            <p>College of Informatics and Computing Sciences</p>
+            <p>College of Teacher Education</p>
+        </div>
+    </div>
+</div>
+
 </footer>
