96033c7d8b2d5864a486deacb6baf512be21d58c
jhonchester <jhonchesterguijoba186@gmail.com>
2024-10-25 21:03:05 +0800

    dashboard image size and remove several div

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,7 +62,7 @@
         justify-content: center;
         }
         #banners .big-banners div {
-        min-width: 575px;
+        min-width: 598px;
         margin: 15px;
         height: 300px;
         display: flex;
@@ -129,21 +129,9 @@
         
     </style>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('User Dashboard') }}
-        </h2>
-    </x-slot>
+    
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <section id="banners" class="section-p1">
         <div class="big-banners">
           <div class="big-banners-1">
